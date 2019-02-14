b9d46f4c9f564570cb5bf9c9fa4295546d120985
Apirath Promyan <gamez.apirath_promyan@hotmail.com>
2019-02-14 17:18:24 +0700

    new branch v2.164-alpine

--- init.groovy
+++ init.groovy
@@ -1,2 +1,12 @@
-import jenkins.model.*
-Jenkins.instance.setNumExecutors(5)
+import hudson.model.*;
+import jenkins.model.*;
+Jenkins.instance.setNumExecutors(5);
+
+Thread.start {
+      sleep 10000
+      println "--> setting agent port for jnlp"
+      def env = System.getenv()
+      int port = env['JENKINS_SLAVE_AGENT_PORT'].toInteger()
+      Jenkins.instance.setSlaveAgentPort(port)
+      println "--> setting agent port for jnlp... done"
+}
